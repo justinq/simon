d6cad10fcf3cfdc5b8344ee90a2fefa31d4caa30
Justin Quillinan <justin@justinq.net>
2011-12-21 02:44:10 +0000

    Add button highlight and audio, format for mobile/touch devices

---
+++ game/game.js
@@ -0,0 +1,49 @@
+/*
+ * Check for touch-enabled browser (this should go in its own file)
+ */
+var check_touch_device = function() {  
+    try {  
+        document.createEvent("TouchEvent");  
+        return true;  
+    } catch (e) {  
+        return false;  
+    }  
+}
+var is_touch_device = check_touch_device();
+
+var opacityOff = 0.5
+  , opacityOn  = 1.0
+  ;
+
+var btnDown = function(element, d, i) {
+    document.getElementById(d+"-tone").play();
+    d3.select(element).style("fill-opacity", opacityOn);
+};
+var btnUp = function(element, d, i) {
+    d3.select(element).style("fill-opacity", opacityOff);
+};
+
+d3.xml("images/game.svg", "image/svg+xml", function(xml) {
+    var importedNode = document.importNode(xml.documentElement, true);
+    d3.select("#game-viz").node().appendChild(importedNode);
+
+    var buttons = [ "centre"
+                  , "blue"
+                  , "yellow"
+                  , "green"
+                  , "red"
+                  ];
+
+    // Bind data and functions to buttons
+    d3.select("#simon").selectAll(".btn")
+      .data(buttons)
+      .style("fill-opacity", opacityOff)
+      .on(is_touch_device ? "touchstart" : "mousedown", function(d, i) {
+          btnDown(this, d, i);
+      })
+      .on(is_touch_device ? "touchend" : "mouseup", function(d, i) {
+          btnUp(this, d, i);
+      });
+
+});
+
